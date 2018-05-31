10f2b5303671a96ea3c1682cce8788dc41944756
Justice Paraiso-Caceres <justiceparaisocace@students.berkeley.net>
2018-05-31 11:27:08 -0700

    g

--- Mapdata.js
+++ Mapdata.js
@@ -1,7 +1,7 @@
 var addresses=[{lat:37.8713659, lng: -122.26756360000002},{lat:37.87024, lng: -122.26711999999998},{lat:37.873984,
     lng: -122.268834}, {lat:37.872238, lng: -122.268868},{lat:37.871840, lng: -122.272126}, {lat: 37.869771, lng:-122.266027},
-    {lat: 37.870726, lng:-122.268427} ];
-var names=["Saigon","Halal Guys","Sweet Green", "Tender Green", "Butcher's Son", "Saturn Cafe", "Flying Falafel"];
+    {lat: 37.870726, lng:-122.268427},{lat: 37.871716,lng:-122.273230}];
+var names=["Saigon","Halal Guys","Sweet Green", "Tender Green", "Butcher's Son", "Saturn Cafe", "Flying Falafel","Trader Joe's"];
 
 function initMap() {
     var myStyles =[
@@ -13,8 +13,6 @@ function initMap() {
             ]
         }
     ];
-
-
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat:37.8715926, lng: -122.272747},
         mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -97,5 +95,13 @@ function initMap() {
     var marker8 = new google.maps.Marker({  position: {lat: 37.870726, lng:-122.268427},content:"Flying Falafel", map: map});
     marker8.addListener('click', function() {
         infowindow8.open(map,marker8);
+    });
+
+    var infowindow9 = new google.maps.InfoWindow({
+        content: "Trader Joe's<br><button>Calculate</button><button id='exit'>x</button>"
+    });
+    var marker9 = new google.maps.Marker({  position: {lat: 37.871716,lng:-122.273230},content:"Trader Joe's", map: map});
+    marker9.addListener('click', function() {
+        infowindow9.open(map,marker9);
     });
 }
